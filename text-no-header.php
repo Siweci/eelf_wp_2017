d7672d26ea5778b5237ea502869e5a5cde91550b
Jean-Marc <montout.jeanmarc@gmail.com>
2018-05-13 17:48:03 +0200

    Add template for example for page confession-de-foi

---
+++ text-no-header.php
@@ -0,0 +1,35 @@
+<?php /* Template Name: text-no-header */  ?>
+
+<?php get_header(); ?>
+
+<header id="only-text-header" class="header">
+            
+    <div class="text-vertical-center">
+
+        <h1><?php the_title(); ?></h1> 
+
+    </div>
+
+</header>
+
+
+<div class="container">
+    
+    <div class="row">
+        
+        <div id="<?php echo $post->post_name; ?>" class="col-xs-12 col-md-10 col-md-offset-1">
+            
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            
+                <?php the_content(); ?>
+            
+            <?php endwhile; endif; ?>
+            
+        </div>
+        
+    </div>
+    
+</div><!-- .container -->
+
+
+<?php get_footer();
