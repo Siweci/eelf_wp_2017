e9bf21dc356a6c509d5f99696cb5c30c45eb4c68
Jean-Marc <montout.jeanmarc@gmail.com>
2018-05-21 16:52:30 +0200

    Initial commit

---
+++ page-nos-activites.php
@@ -0,0 +1,90 @@
+<?php get_header();?>
+
+<?php $page_slug = $post->post_name; ?>
+
+<?php get_template_part( 'parts/header-no-text' ); ?>
+
+<?php wp_reset_postdata(); ?>
+<?php
+$args = array(
+    'post_type' => 'activites',
+    'orderby' => 'menu_order',
+    'order' => 'ASC'
+);
+
+$activites = new WP_Query( $args );
+?>
+
+<div class="container">
+
+    <div class="row">
+        
+        <section id="<?php echo $page_slug; ?>" class="col-xs-12">
+
+            <div class="row">
+
+                <?php if( $activites->have_posts() ) : while ( $activites->have_posts() ) : $activites->the_post(); ?>
+
+                    <article id="culte-dimanche" class="col-xs-12 col-md-4 text-center">
+
+                        <div class="panel panel-default panel-no-bg panel-no-border-in">
+
+                            <div class="panel-heading text-center">
+
+                                <?php the_post_thumbnail(
+                                            'galerie-activites',
+                                            array('class' => 'img-responsive') );
+                                ?>
+                                <h3 class="panel-title"><?php the_title(); ?></h3>
+
+                            </div>
+
+                            <div class="panel-body">
+                               <?php the_content(); ?>
+                            </div>
+
+                            <div class="panel-footer">
+                                
+                                <?php
+                                
+                                /*
+                                 * Pour les metas
+                                 * mmprime en priorité le lien ou le texte
+                                 */
+                                
+                                $link = get_field( 'link_activite' );
+                                
+                                if ( empty( $link ) ) {
+                                    
+                                    the_field( 'meta_activite' );
+                                    
+                                } else {
+                                    
+                                    printf('<a href="%1$s" target="%2$s">%3$s</a>',
+                                            $link['url'],
+                                            $link['target'],
+                                            $link['title']
+                                    );
+                                    
+                                }
+                                ?>
+                                
+                            </div>
+
+                        </div>
+
+                    </article>
+
+                <?php endwhile; endif; ?>
+                
+            </div>
+            
+        </section>
+
+    </div><!-- .row -->
+
+</div><!-- .container-fluid -->
+
+
+
+<?php get_footer();
---
+++ parts/header-no-text.php
@@ -0,0 +1,9 @@
+<header id="only-text-header" class="header">
+         
+    <div class="text-vertical-center">
+
+        <h1><?php the_title(); ?></h1>
+        
+    </div>
+
+</header>
