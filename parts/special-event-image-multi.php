3ec098b2ca8487d0206a7a2b8a1bcd929d8b131b
Jean-Marc <montout.jeanmarc@gmail.com>
2019-03-11 11:14:07 +0100

    Change le id en classe

--- parts/special-event-image-multi.php
+++ parts/special-event-image-multi.php
@@ -13,7 +13,7 @@
 
 <div class="col-xs-12 col-md-4 col-md-offset-4 text-center">
     
-    <a href="<?php echo the_permalink(); ?>" id="special-event-more"
-       class="more btn btn-lg btn-green">Lire plus</a>
+    <a href="<?php echo the_permalink(); ?>" 
+       class="special-event-more more btn btn-lg btn-green">Lire plus</a>
        
 </div>
