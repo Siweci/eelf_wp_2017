61e7a52ec37e3ec1fb739b1b1cd2a94c081b6f04
Jean-Marc <montout.jeanmarc@gmail.com>
2019-03-11 19:20:16 +0100

    Page category-evenements-speciaux

--- category-evenements-speciaux.php
+++ category-evenements-speciaux.php
@@ -1,8 +1,46 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * 
  */
 
+get_header();
+
+?>
+
+<div id="page-evenements-speciaux" class="main">
+    
+    <div class="container-fluid">
+        
+        <div class="row">
+            
+            <h1 class="text-center grey-9 col-xs-12">Evénements exceptionnels</h1>
+            
+            <section class="col-xs-12 col-md-10 col-md-offset-1">
+                
+                <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+                
+                    <div id="<?php echo "special-event-" . get_the_ID(); ?>" <?php post_class( 'special-event col-xs-12 col-md-4'); ?>>
+
+                        <div class="row">
+
+                            <?php
+
+                            get_template_part ( 'parts/special-event-image', 'multi' );
+
+                            ?>
+
+                        </div>
+
+                    </div>
+                
+                <?php endwhile; endif; ?>
+                
+            </section>
+            
+        </div>
+        
+    </div>
+    
+</div>
+
+<?php get_footer();
